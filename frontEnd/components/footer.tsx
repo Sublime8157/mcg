ad5987667c36e1c344e36eea6c03b014a378c369
Joven Miran <joven.miran@jeonsoft.com>
2025-05-29 14:20:42 +0800

    Support arrow up functionality on footer

--- frontEnd/components/footer.tsx
+++ frontEnd/components/footer.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useRef, useEffect, useState} from 'react';
+import { MdArrowCircleUp } from "react-icons/md";
 
 const Footer: React.FC = () => {
+    const targetRef = useRef<HTMLDivElement>(null)
+    const [isVisible, setIsVisible] = useState(false)
+
+    useEffect(() => {
+        if(!isVisible){
+            const observer = new IntersectionObserver(
+                ([entry]) => {
+                    setIsVisible(entry.isIntersecting)
+                }, 
+                { threshold: 0.5 }
+            ); 
+
+            const current = targetRef.current
+            if(current) observer.observe(current)
+            
+            return () => {
+                if(current) observer.unobserve(current)
+            }
+        }
+    }, [])
+    
     return (
-        <div className='h-screen w-full flex flex-col items-center justify-center gap-8 featuredDelicacies'>
+        <div ref={targetRef} className='h-screen w-full flex flex-col items-center justify-center gap-8 featuredDelicacies'>
            <h1 className='text-5xl text-pink-300' style={{ fontWeight: 900 }}>Order Now</h1>
            <img src='/images/testImage0.png'></img>
            <p className='text-white text-lg w-6/12 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto nihil ad sapiente quibusdam vel laboriosam architecto aperiam facilis esse quae aliquam aliquid fugit pariatur assumenda voluptatibus ab a, tempora recusandae.</p>
@@ -20,6 +42,7 @@ const Footer: React.FC = () => {
                 <div className='text-lg text-white' >Copyright © { new Date().getFullYear() } MCG</div>
                 <div className='text-lg text-white' >Powered By MCG</div>
             </div>
+            <div onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' }) } className={`animate fixed bottom-0 right-0 p-10 text-3xl hover:text-4xl cursor-pointer ${isVisible ? `opacity-100 translate-x-0` : `opacity-0 translate-x-20`}`}><MdArrowCircleUp /></div>
         </div>
     )
 }
--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -6,7 +6,7 @@ import Menu from "../components/menu/ourMenuIndex"
 import ContactUs from "../components/contact_us/contactUsIndex"
 import Footer from "../components/footer"
 import {Routes, Route } from "react-router-dom";
-import { MdArrowCircleUp } from "react-icons/md";
+
 // import Browser
 const App: React.FC = () => {
   return (
@@ -19,7 +19,6 @@ const App: React.FC = () => {
             <Route path ="/contactUs" element={<ContactUs />} />
         </Routes>
       <Footer />
-      <div className={`fixed bottom-0 right-0 p-10 text-3xl hover:text-4xl cursor-pointer`}><MdArrowCircleUp /></div>
     </>
   )  
 }
